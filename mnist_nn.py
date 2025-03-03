8110262695188c8d32dcbbbea1acb0a401d1956f
Bung1e <leshkachovzhik@gmail.com>
2025-03-03 15:23:56 +0100

    practice with valuation

--- mnist_nn.py
+++ mnist_nn.py
@@ -1,7 +1,9 @@
 import os 
 import json
 from PIL import Image
+import matplotlib.pyplot as plt
 
+import torchvision
 import torch
 import torch.utils.data as data
 import torchvision.transforms.v2 as tfs
@@ -62,7 +64,7 @@ class DigitNN(nn.Module):
 
 model = DigitNN(28 * 28, 32, 10)
 
-state_dict = torch.load('edu/Pytorch/model_dnn.tar', weights_only=True) # loading states of nn
+state_dict = torch.load(r'edu/PyTorch/model_dnn.tar', weights_only=True) # loading states of nn
 model.load_state_dict(state_dict) # loading states to model
 
 transforms = tfs.Compose([tfs.ToImage(), tfs.Grayscale(), tfs.ToDtype(torch.float32, scale=True), tfs.Lambda(lambda _img: _img.ravel())]) 
@@ -71,19 +73,32 @@ transforms = tfs.Compose([tfs.ToImage(), tfs.Grayscale(), tfs.ToDtype(torch.floa
 # а у нас серое изображение поэтому к такому размеру и надо привести, в итоге получаем [1, 28, 28]
 # scale означает что мы нормируем изображение (приводим в шкалу от 0 до 1)
 
-d_train = ImageFolder("edu/PyTorch/dataset/train", transform=transforms)  
+# d_train = ImageFolder("edu/PyTorch/dataset/train", transform=transforms)  
 # используем ImageFolder вместо целого класса DigitDataset написаного ранее, функция ImageFolder делает тоже самое
 
-train_data = data.DataLoader(d_train, batch_size=32, shuffle=True)   
-print(len(train_data))
+# train_data = data.DataLoader(d_train, batch_size=32, shuffle=True)   
+# print(len(train_data))
+
+dataset_mnist = torchvision.datasets.MNIST(r'edu\PyTorch\datasets\mnist', download=True, train=True, transform=transforms)
+d_train, d_val = data.random_split(dataset_mnist, [0.7, 0.3])
+train_data = data.DataLoader(d_train, batch_size=32, shuffle=True)
+train_data_val = data.DataLoader(d_val, batch_size=32, shuffle=False)
+# когда проверяем перемешивать не надо
+
 
 optimizer = optim.Adam(params=model.parameters(), lr=0.01)
 loss_function = nn.CrossEntropyLoss()
+epochs = 15
 
-epochs = 0
-model.train()
+loss_lst_val = [] # список значений потерь при валидации
+loss_lst = [] # список значений потерь при обучении
+
 for _e in range(epochs):
-    train_tqdm = tqdm(train_data, leave=True)
+    model.train()
+    loss_mean = 0
+    lm_count = 0
+
+    train_tqdm = tqdm(train_data, leave=False)
     for x_train, y_train in train_data:
         predict = model(x_train)
         loss = loss_function(predict, y_train)
@@ -91,7 +106,31 @@ for _e in range(epochs):
         loss.backward()
         optimizer.step()
 
-d_test = ImageFolder("edu/PyTorch/dataset/test", transform=transforms)  
+        lm_count += 1
+        loss_mean = 1/lm_count * loss.item() + (1 - 1/lm_count) * loss_mean
+        train_tqdm.set_description(f"Epoch [{_e+1}/{epochs}], loss_mean={loss_mean:.3f}")
+
+    model.eval()
+    Q_val = 0
+    count_val = 0
+
+    for x_val, y_val in train_data_val:
+        with torch.no_grad():
+            p = model(x_val)
+            loss = loss_function(p, y_val)
+            Q_val += loss.item()
+            count_val += 1
+
+    Q_val /= count_val
+
+    loss_lst.append(loss_mean)
+    loss_lst_val.append(Q_val)
+
+    print(f" | loss_mean={loss_mean:.3f}, Q_val={Q_val:.3f}")
+
+
+
+d_test = ImageFolder(r'edu/PyTorch/dataset/test', transform=transforms)  
 test_data = data.DataLoader(d_test, batch_size=500, shuffle=False)  #перемешивание отключено потому что для тестирования это не имеет значения
 Q = 0
 model.eval()
@@ -111,3 +150,8 @@ print(Q)
 # torch.save(st, 'edu/pytorch/model_dnn.tar')
 # state_dict = torch.load('model_dnn.tar', weights_only=True) loading states of nn
 # model.load_state_dict(state_dict) loading states to model
+
+plt.plot(loss_lst)
+plt.plot(loss_lst_val)
+plt.grid()
+plt.show()
